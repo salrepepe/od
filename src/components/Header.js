0576293a29985fa827c78488599c70304d3ef68b
Mukhammed-Said Kuturov <salrepepe@Mukhammed-Saids-MacBook-Air.local>
2021-05-14 13:22:29 +0600

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,22 +6,20 @@ import instagram from '../images/header/instagram.svg';
 const Header = () => {
   return (
     <header className="header">
-      <div className="container">
-        <nav className="nav d-flex justify-content-between">
+      <div className="container d-flex justify-content-between">
+        <nav className="nav">
           <div className="d-flex">
             <img src={logo} alt="logo" className="header__logo"/>
-            <div className="d-flex"> 
               <a href="#" className="nav-item">О компании</a>
               <a href="#" className="nav-item">Услуги</a>
               <a href="#" className="nav-item">Наши работы</a>
               <a href="#" className="nav-item">Контакты</a>
-            </div>
-          </div>
-          <div>
-            <a href=""><img src={instagram} className=" header__img" alt="instagram" /></a>
-            <a href=""><img src={facebook} className=" header__img" alt="facebook" /></a>
           </div>
         </nav>
+        <div> 
+          <a href=""><img src={instagram} className=" header__img" alt="instagram" /></a>
+          <a href=""><img src={facebook} className=" header__img" alt="facebook" /></a>
+        </div>
       </div>
     </header>
   );
